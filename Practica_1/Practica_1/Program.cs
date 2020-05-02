ec71c28fb636b8fde1f6cd8738ccbb67322b5d3c
LuisDiaz0307 <63927128+LuisDiaz0307@users.noreply.github.com>
2020-05-02 06:48:15 -0700

    Practicas para Estructura de Datos

---
+++ Practica_1/Practica_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mes = { "Enero" , "Febrero" , "Marzo" , "Abril" , "Mayo" , "Junio" , "Julio" ,
+                "Agosto" , "Septiembre" , "Octubre" , "Noviembre" , "Diciembre" };
+
+            Console.Write("Introduzca el número del mes: ");
+            int NumMes = 0;
+            Int32.TryParse(Console.ReadLine(), out NumMes);
+
+            if (NumMes < 1 || NumMes > 12)
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                    Console.WriteLine(mes[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Seleccione el mes de: " + mes[NumMes - 1]);
+                
+            }
+
+        }
+    }
+}
---
+++ Practica_2/Practica_2/Practica_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)//[] para hacer arreglos uni, [,] para arreglos de mas dimensiones
+        {
+            /*
+            Instrucciones : Hacer una aplicación en C# de consola, donde se solicite ingresar nombre de 10 personas,
+            al terminar de ingresar esas 10 personas, debe de mostrar una lista con los nombres ingresados.
+            */
+            string[] Nombres = new string[10];
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.Write($"Escriba aquí el nombre de la persona { i + 1 } :");
+                Nombres[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.WriteLine("El nombre de persona " + (i + 1) + " es: " + Nombres[i]);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
+// Luis Diaz Practica_2
